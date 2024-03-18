b5994b381fbd15b765375e96d9d390053d88ea7b
mar1a3 <popytka.nomer1@bk.ru>
2024-03-18 17:10:06 +0300

    Внесла правки

--- src/components/Select/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const SelectWraper = styled.div`
-    color:#424242;
-    margin-bottom:30px;
-    & p{
-        font-size:20px;
-    }
-`
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Header } from '../components/Header/Header'
 import { PersonalInfo } from '../components/PersonalInfo/PersonalInfo';
-import { Education } from '../components/Education/Education';
 
 export const MainPage = () => {
   return (
     <>
       <Header />
       <PersonalInfo />
-      <Education />
     </>
   )
 }
