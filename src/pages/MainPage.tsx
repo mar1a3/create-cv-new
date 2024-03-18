0f0c692a6974e12da4bb4d589c0918696145d852
mar1a3 <popytka.nomer1@bk.ru>
2024-03-18 17:19:41 +0300

    Education

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from '../components/Header/Header'
 import { PersonalInfo } from '../components/PersonalInfo/PersonalInfo';
+import { Education } from '../components/Education/Education';
 
 export const MainPage = () => {
   return (
     <>
       <Header />
       <PersonalInfo />
+      <Education/>
     </>
   )
 }
