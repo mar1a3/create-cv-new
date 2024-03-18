bedbe98db3e751b217cff7b349adbf4324136d8c
mar1a3 <popytka.nomer1@bk.ru>
2024-03-18 13:21:36 +0300

    Removed img

--- src/components/Calendar/CustomCalendar.tsx
+++ src/components/Calendar/CustomCalendar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { DatePickerProps } from 'antd';
-import { DatePicker, Space } from 'antd';
+import { DatePicker } from 'antd';
 import {styled, createGlobalStyle} from 'styled-components';
 
 export const CardFont = createGlobalStyle`
