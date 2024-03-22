1c01d683c8da79f627ade4b8261ef284027632a0
mar1a3 <popytka.nomer1@bk.ru>
2024-03-22 08:01:36 +0300

    удалила закомментированное

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -3,7 +3,6 @@ import { InfoCard } from '../InfoCard/InfoCard';
 import { FormControl } from '../FormControl/FormControl';
 import { Input } from '../Input/Input';
 import { DatePicker } from 'antd';
-import { CalendarWidget } from '../CalendarWidget/CalendarWidget';
 import { Select } from '../Select/Select'
 
 export const Education = () => {
@@ -13,11 +12,10 @@ export const Education = () => {
                 <Input placeholder="Фио" />
             </FormControl>
             <FormControl label="Дата окончания">
-            <DatePicker
-                placeholder={"Дата окончания"}
-            />
+                <DatePicker
+                    placeholder={"Дата окончания"}
+                />
             </FormControl>
-            {/* <CalendarWidget title="Дата окончания" placeholder="Дата окончания" /> */}
             <Select
                 defaultValue="Бакалавр"
                 title="Степень"
